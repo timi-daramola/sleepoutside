e97c14058d23e300644a3b120012a1ae9f60abb0
Timilehin Daramola <timi.homezone111@gmail.com>
2025-01-31 09:13:49 -0500

    WDD330

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,8 +1,15 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+import ProductData from './ProductData.js';
+import ProductList from './ProductList.js';
+import { loadHeaderFooter, getParam } from './utils.mjs';
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("Tents", dataSource, element);
+loadHeaderFooter();
 
-listing.init();
+const category = getParam('category');
+// first create an instance of our ProductData class.
+const dataSource = new ProductData();
+// then get the element we want the product list to render in
+const listElement = document.querySelector('.product-list');
+// then create an instance of our ProductList class and send it the correct information.
+const myList = new ProductList(category, dataSource, listElement);
+// finally call the init method to show our products
+myList.init();
